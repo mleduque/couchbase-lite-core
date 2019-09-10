514b5efe50386e51eee0ada02f10ee0761544ab7
Evgeniy A. Dushistov <dushistov@mail.ru>
2019-09-10 23:38:44 +0300

    make possible to set logger from Rust
    va_list is not supported yet, see
    https://github.com/rust-lang/rust/issues/44930

--- C/c4Base.cc
+++ C/c4Base.cc
@@ -323,6 +323,21 @@ void c4log_writeToCallback(C4LogLevel level, C4LogCallback callback, bool prefor
     LogDomain::setCallback((LogDomain::Callback_t)callback, preformatted);
     LogDomain::setCallbackLogLevel((LogLevel)level);
 }
+
+static std::atomic<C4LogCallbackR> g_rust_log_callback{nullptr};
+
+static void rust_log_callback(C4LogDomain d, C4LogLevel l,
+                              const char *fmt C4NONNULL, va_list) {
+  C4LogCallbackR rust_log_callback = g_rust_log_callback.load(std::memory_order_relaxed);
+  if (rust_log_callback != nullptr) {
+    rust_log_callback(d, l, fmt);
+  }
+}
+
+void c4log_setRustCallback(C4LogLevel level, C4LogCallbackR callback) noexcept {
+  g_rust_log_callback.store(callback, std::memory_order_relaxed);
+  c4log_writeToCallback(level, rust_log_callback, true);
+}
 // LCOV_EXCL_STOP
 
 bool c4log_writeToBinaryFile(C4LogFileOptions options, C4Error *outError) noexcept {
